17e9363b3f695be4aa943508f1aaf9647f161005
Liam Hession <liam@gobootler.com>
2020-06-10 22:43:00 -0400

    Adds test write-firestore function for demo purposes

--- functions/write-firestore/write-firestore.js
+++ functions/write-firestore/write-firestore.js
@@ -1,13 +1,28 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://workflow-edu.firebaseio.com'
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+const db = admin.firestore();
+
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const newOnboardingResponse = db.collection('onboardingResponses').doc();
+
+    const createdResponse = newOnboardingResponse.set({
+      userId: 'cdkd-4124-dkal',
+      userEmail: 'fake@me.com',
+      favoriteMethods: ['walk', 'meditation'],      
+    });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ createdResponse }),
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
