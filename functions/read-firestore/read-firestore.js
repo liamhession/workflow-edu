37cbb09eec8038e6a81dc1633c73284f04d00baa
Liam Hession <liam@gobootler.com>
2020-06-10 22:06:34 -0400

    Making firestore read work

--- functions/read-firestore/read-firestore.js
+++ functions/read-firestore/read-firestore.js
@@ -1,13 +1,27 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://workflow-edu.firebaseio.com'
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+const db = admin.firestore();
+
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    let allResponses = [];
+    const snapshot = await db.collection('onboardingResponses').get()
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+      allResponses.push(doc.data());
+    });
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ onboardingResponses: allResponses }),
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
