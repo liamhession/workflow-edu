f99856a7d3b4dca6da72d92a1872e959265dc7f3
Liam Hession <liam@gobootler.com>
2020-06-10 22:15:42 -0400

    Fully removing identity widget, all its dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-postcss`,
-    {
-      resolve: `gatsby-plugin-netlify-identity`,
-      options: {
-        url: `https://boring-brown-d07175.netlify.app/` // required!
-      }
-    }
+    `gatsby-plugin-postcss`
   ]
 };
+// ,
+//     {
+//       resolve: `gatsby-plugin-netlify-identity`,
+//       options: {
+//         url: `https://boring-brown-d07175.netlify.app/` // required!
+//       }
+//     }
